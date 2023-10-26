b9267b437f991e3b5bcb8d5b43f039c29b8ea587
rachel <jrachelr1@gmail.com>
2023-10-26 09:59:47 -0700

    fixed spacing in nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="bg-blue-600 p-4 sticky top-0 drop-shadow-xl z-10">
-      <div className="prose prose-xl mx-auto flex justify-between flex-col sm:flex-row">
+      <div className="prose mx-auto flex justify-between flex-col sm:flex-row">
         <h1 className="text-2xl font-bold text-white grid place-content-center mb-2 md:mb-0">
           <Link
             href="/"
