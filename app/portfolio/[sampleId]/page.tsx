479d315f642d6bedc7fc885289ffbb3524fe0e9e
rachel <jrachelr1@gmail.com>
2024-04-05 18:55:15 -0700

    creating iframe component

--- app/portfolio/[sampleId]/page.tsx
+++ app/portfolio/[sampleId]/page.tsx
@@ -1,4 +1,5 @@
 import samples from "../../../public/data/work-samples.json";
+import Link from "next/link";
 
 type Params = {
   params: { sampleId: number };
@@ -7,10 +8,22 @@ type Params = {
 export default function Page({ params: { sampleId } }: Params) {
   const numID = Number(sampleId);
   const data = samples.find((sample) => sample.id === numID);
-  // sampleID is a num
-  console.log(data);
-  console.log(sampleId);
-  console.log(samples);
 
-  return <section>{}</section>;
+  return (
+    <div className="p-6 prose prose-l prose-slate dark:prose-invert mx-auto">
+      <h2 className="text-xl">{data?.title}</h2>
+      <p>{data?.url}</p>
+      <section>
+        <iframe
+          id={data?.url}
+          title={data?.title}
+          className="aspect-auto w-full"
+          src={data?.url}
+        />
+      </section>
+      <p className="prose prose-sm prose-slate dark:prose-invert mx-auto">
+        <Link href="/portfolio">← Back to all</Link>
+      </p>
+    </div>
+  );
 }
---
+++ components/PDFViewer.tsx
@@ -0,0 +1,9 @@
+import samples from "../public/data/work-samples.json";
+
+export default async function PDFViewer() {
+  return (
+    <section className="mt-6 mx-auto max-w-2xl">
+      <iframe />
+    </section>
+  );
+}
